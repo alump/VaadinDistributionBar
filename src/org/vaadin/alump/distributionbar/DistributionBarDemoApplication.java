e493aa756d2eef9912acc8b803315bf5c5f3a9ca
Sami Viitanen <sami.viitanen@gmail.com>
2012-01-25 15:00:07 +0200

    Demo application added

--- src/org/vaadin/alump/distributionbar/DistributionBarDemoApplication.java
+++ src/org/vaadin/alump/distributionbar/DistributionBarDemoApplication.java
@@ -18,17 +18,132 @@
 
 package org.vaadin.alump.distributionbar;
 
+import java.util.Random;
+
 import com.vaadin.Application;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 public class DistributionBarDemoApplication extends Application {
+	
+	private DistributionBar barOne;
+	private DistributionBar barTwo;
+	private DistributionBar barThree;
+	private DistributionBar barFour;
+	private DistributionBar barFive;
+	
+	final static private int BAR_ONE_PARTS = 2;
+	final static private int BAR_TWO_PARTS = 3;
+	final static private int BAR_THREE_PARTS = 6;
+	final static private int BAR_FOUR_PARTS = 10;
+	final static private int BAR_FIVE_PARTS = 2;
+	
 	@Override
 	public void init() {
+		
+		setTheme ("distributionbardemo");
+		
 		Window mainWindow = new Window("Vaadin Distribution Bar Demo");
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
+		mainWindow.addComponent(buildView());
 		setMainWindow(mainWindow);
 	}
+	
+	private Component buildView() {
+		
+		VerticalLayout layout = new VerticalLayout();
+		layout.setSizeFull();
+		layout.setSpacing(true);
+		
+		Label header = new Label ("Distribution Bar Demo");
+		layout.addComponent(header);
+		
+		Button randomButton = new Button("Click here to update the bars values");
+		randomButton.addListener(randomButtonListener);
+		layout.addComponent(randomButton);
+		
+		barOne = new DistributionBar(BAR_ONE_PARTS);
+		barOne.setCaption("Senate:");
+		barOne.setWidth("100%");
+		barOne.addStyleName("my-bar-one");
+		barOne.setPartTitle(0, "Democratic Party");
+		barOne.setPartTitle(1, "Republican Party");
+		layout.addComponent(barOne);
+		layout.setComponentAlignment(barOne, Alignment.MIDDLE_CENTER);
+		
+		barTwo = new DistributionBar(BAR_TWO_PARTS);
+		barTwo.setCaption("Do people like nicer backgrounds?");
+		barTwo.setWidth("100%");
+		barTwo.addStyleName("my-bar-two");
+		layout.addComponent(barTwo);
+		layout.setComponentAlignment(barTwo, Alignment.MIDDLE_CENTER);
+		
+		barThree = new DistributionBar(BAR_THREE_PARTS);
+		barThree.setCaption("Maaaany parts with default styling");
+		barThree.setWidth("100%");
+		barThree.addStyleName("my-bar-three");
+		layout.addComponent(barThree);
+		layout.setComponentAlignment(barThree, Alignment.MIDDLE_CENTER);
+		
+		barFour = new DistributionBar(BAR_FOUR_PARTS);
+		barFour.setCaption("CSS tricks");
+		barFour.setWidth("100%");
+		barFour.addStyleName("my-bar-four");
+		layout.addComponent(barFour);
+		layout.setComponentAlignment(barFour, Alignment.MIDDLE_CENTER);
+		
+		barFive = new DistributionBar(BAR_FIVE_PARTS);
+		barFive.setCaption("Vote results:");
+		barFive.setWidth("100%");
+		barFive.addStyleName("my-bar-five");
+		barFive.setPartTitle(0, "YES!");
+		barFive.setPartTitle(1, "NO!");
+		layout.addComponent(barFive);
+		layout.setComponentAlignment(barFive, Alignment.MIDDLE_CENTER);
+
+		
+		return layout;
+		
+	}
+	
+	private final Button.ClickListener randomButtonListener = 
+		new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				
+				Random random = new Random();
+				
+
+				int chairs = 100;
+				int groupA = random.nextInt(chairs);
+				
+				barOne.setPartSize(0, groupA);
+				barOne.setPartSize(1, chairs - groupA);
+
+				
+				for (int i = 0; i < BAR_TWO_PARTS; ++i) {
+					barTwo.setPartSize(i, random.nextInt(20));
+				}
+				
+				for (int i = 0; i < BAR_THREE_PARTS; ++i) {
+					barThree.setPartSize(i, random.nextInt(50));
+				}
+				
+				for (int i = 0; i < BAR_FOUR_PARTS; ++i) {
+					barFour.setPartSize(i, random.nextInt(10));
+				}
+				
+				for (int i = 0; i < BAR_FIVE_PARTS; ++i) {
+					barFive.setPartSize(i, random.nextInt(10000000));
+				}
+				
+			}
+	};
+	
+	
 
 }
