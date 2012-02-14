f6745bc19d8a62e81f40538b81050e084b2124a5
Sami Viitanen <sami.viitanen@vaadin.com>
2012-02-14 12:56:57 +0200

    One more unserializable class left behind

--- src/org/vaadin/alump/distributionbar/DistributionBar.java
+++ src/org/vaadin/alump/distributionbar/DistributionBar.java
@@ -17,6 +17,7 @@
  */
 package org.vaadin.alump.distributionbar;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -34,81 +35,86 @@ import com.vaadin.ui.AbstractComponent;
  */
 @com.vaadin.ui.ClientWidget(org.vaadin.alump.distributionbar.widgetset.client.ui.VDistributionBar.class)
 public class DistributionBar extends AbstractComponent {
-	
+
 	private static final long serialVersionUID = -3585161316003999470L;
 	private List<Part> parts;
-	
+
 	/**
 	 * Internal storage class for part details
 	 */
-	private static class Part {
-		
+	private static class Part implements Serializable {
+
+		private static final long serialVersionUID = 4009516719242009675L;
 		private int size;
 		private String title;
 		private String tooltip;
-		
+
 		public Part() {
 			title = new String();
 			tooltip = new String();
 		}
-		
+
 		@SuppressWarnings("unused")
 		public Part(int size) {
-			setSize (size);
+			setSize(size);
 			title = new String();
 		}
-		
-		public void setSize (int size) {
+
+		public void setSize(int size) {
 			this.size = Math.abs(size);
 		}
-		
-		public int getSize () {
+
+		public int getSize() {
 			return size;
 		}
-		
-		public void setTitle (String title) {
+
+		public void setTitle(String title) {
 			this.title = title;
 		}
-		
-		public String getTitle () {
+
+		public String getTitle() {
 			return title;
 		}
-		
-		public void setTooltip (String tooltip) {
+
+		public void setTooltip(String tooltip) {
 			this.tooltip = tooltip;
 		}
-		
-		public String getTooltip () {
+
+		public String getTooltip() {
 			return tooltip;
 		}
 	}
-	
+
 	/**
 	 * Will make distribution bar with 2 parts (size value zero).
 	 */
 	public DistributionBar() {
 		this(2);
 	}
-	
+
 	/**
 	 * Generate distribution bar with given number of parts. All parts created
 	 * will get the default size value: zero.
-	 * @param numberOfParts Number of parts. Must be two or more. Smaller
-	 * values are converted to two.
+	 * 
+	 * @param numberOfParts
+	 *            Number of parts. Must be two or more. Smaller values are
+	 *            converted to two.
 	 */
 	public DistributionBar(int numberOfParts) {
 		parts = createPartsList(numberOfParts);
 	}
-	
+
 	/**
 	 * Create new distribution bar and define sizes for all parts
-	 * @param sizes Part sizes in integer array. Must have at least 2 sizes. If
-	 * less two parts are made with size zero.
+	 * 
+	 * @param sizes
+	 *            Part sizes in integer array. Must have at least 2 sizes. If
+	 *            less two parts are made with size zero.
 	 */
 	public DistributionBar(final int[] sizes) {
-		
+
 		parts = createPartsList(sizes.length);
-		
+
 		for (int i = 0; i < sizes.length; ++i) {
 			parts.get(i).size = sizes[i];
 		}
@@ -117,16 +123,14 @@ public class DistributionBar extends AbstractComponent {
 	@Override
 	public void paintContent(PaintTarget target) throws PaintException {
 		super.paintContent(target);
-		
+
 		target.addAttribute("parts", parts.size());
 		for (int i = 0; i < parts.size(); ++i) {
 			Part part = parts.get(i);
-			target.addAttribute ("psize-" + String.valueOf(i),
-				part.getSize());
-			target.addAttribute ("ptitle-" + String.valueOf(i),
-				part.getTitle());
-			target.addAttribute ("ptooltip-" + String.valueOf(i),
-				part.getTooltip());
+			target.addAttribute("psize-" + String.valueOf(i), part.getSize());
+			target.addAttribute("ptitle-" + String.valueOf(i), part.getTitle());
+			target.addAttribute("ptooltip-" + String.valueOf(i),
+					part.getTooltip());
 		}
 	}
 
@@ -139,86 +143,104 @@ public class DistributionBar extends AbstractComponent {
 	public void changeVariables(Object source, Map<String, Object> variables) {
 		super.changeVariables(source, variables);
 	}
-	
+
 	/**
-	 * Update multiple sizes once. If given list is smaller than number of
-	 * parts then parts at the end will not be updated. If given list has more
-	 * sizes than there is parts, then rest of the sizes are ignored.
-	 * @param partSizes Sizes in integer array
+	 * Update multiple sizes once. If given list is smaller than number of parts
+	 * then parts at the end will not be updated. If given list has more sizes
+	 * than there is parts, then rest of the sizes are ignored.
+	 * 
+	 * @param partSizes
+	 *            Sizes in integer array
 	 */
-	public void updatePartSizes (int[] partSizes) {
-		
+	public void updatePartSizes(int[] partSizes) {
+
 		for (int i = 0; i < partSizes.length && i < parts.size(); ++i) {
 			parts.get(i).size = partSizes[i];
 		}
-		
+
 		requestRepaint();
 	}
-	
+
 	/**
 	 * Setup part by defining both size and tooltip with one command
-	 * @param index Index of part [0..N]. Only give valid indexes.
-	 * @param size Size as integer number
-	 * @param tooltip Tooltip content is XHTML
+	 * 
+	 * @param index
+	 *            Index of part [0..N]. Only give valid indexes.
+	 * @param size
+	 *            Size as integer number
+	 * @param tooltip
+	 *            Tooltip content is XHTML
 	 */
-	public void setupPart (int index, int size, String tooltip) {
-		Part part = parts.get(index); 
+	public void setupPart(int index, int size, String tooltip) {
+		Part part = parts.get(index);
 		part.setSize(size);
 		part.setTooltip(tooltip);
 		requestRepaint();
 	}
-	
+
 	/**
 	 * Change size of given part
-	 * @param index Index of part [0..N]. Only give valid indexes.
-	 * @param size Size as integer number
+	 * 
+	 * @param index
+	 *            Index of part [0..N]. Only give valid indexes.
+	 * @param size
+	 *            Size as integer number
 	 */
-	public void setPartSize (int index, int size) {
-		Part part = parts.get(index); 
+	public void setPartSize(int index, int size) {
+		Part part = parts.get(index);
 		part.setSize(size);
 		requestRepaint();
 	}
-	
+
 	/**
-	 * Change title of given part. This is normal HTML title attribute. For
-	 * many use cases tooltip is better option.
-	 * @param index Index of part [0..N]. Only give valid indexes.
-	 * @param title Title for part
+	 * Change title of given part. This is normal HTML title attribute. For many
+	 * use cases tooltip is better option.
+	 * 
+	 * @param index
+	 *            Index of part [0..N]. Only give valid indexes.
+	 * @param title
+	 *            Title for part
 	 */
-	public void setPartTitle (int index, String title) {
-		Part part = parts.get(index); 
+	public void setPartTitle(int index, String title) {
+		Part part = parts.get(index);
 		part.setTitle(title);
 		requestRepaint();
 	}
-	
+
 	/**
 	 * Change tooltip of given part.
-	 * @param index Index of part [0..N]. Only give valid indexes.
-	 * @param tooltip Content of tooltip (empty is do not show tooltip). Content
-	 * is given in XHTML format.
+	 * 
+	 * @param index
+	 *            Index of part [0..N]. Only give valid indexes.
+	 * @param tooltip
+	 *            Content of tooltip (empty is do not show tooltip). Content is
+	 *            given in XHTML format.
 	 */
-	public void setPartTooltip (int index, String tooltip) {
-		Part part = parts.get(index); 
+	public void setPartTooltip(int index, String tooltip) {
+		Part part = parts.get(index);
 		part.setTooltip(tooltip);
 		requestRepaint();
 	}
-	
+
 	/**
 	 * Get number of parts in distribution bar
+	 * 
 	 * @return Number of parts in distribution bar
 	 */
 	public int getNumberOfParts() {
 		return parts.size();
 	}
-	
+
 	/**
 	 * Change number of parts in distribution bar.
-	 * @param numberOfParts New number of parts. If this is different than
-	 * earlier number of parts then all parts will be initialized to default
-	 * state with size zero. If given value is less than 2 it will be converted
-	 * to two.
+	 * 
+	 * @param numberOfParts
+	 *            New number of parts. If this is different than earlier number
+	 *            of parts then all parts will be initialized to default state
+	 *            with size zero. If given value is less than 2 it will be
+	 *            converted to two.
 	 */
-	public void setNumberOfParts(int numberOfParts) {		
+	public void setNumberOfParts(int numberOfParts) {
 		if (parts.size() == numberOfParts) {
 			return;
 		} else if (parts.size() != numberOfParts) {
@@ -226,11 +248,13 @@ public class DistributionBar extends AbstractComponent {
 			requestRepaint();
 		}
 	}
-	
+
 	/**
 	 * Generate new parts lists. All part information is lost.
-	 * @param amount Amount of parts in list. If less than two is given value
-	 * is read as two.
+	 * 
+	 * @param amount
+	 *            Amount of parts in list. If less than two is given value is
+	 *            read as two.
 	 * @return Parts list generated
 	 */
 	private static List<Part> createPartsList(int amount) {
@@ -238,12 +262,12 @@ public class DistributionBar extends AbstractComponent {
 		if (makeParts < 2) {
 			makeParts = 2;
 		}
-		
+
 		List<Part> ret = new ArrayList<Part>();
 		for (int i = 0; i < makeParts; ++i) {
 			ret.add(new Part());
 		}
-		
+
 		return ret;
 	}
 
